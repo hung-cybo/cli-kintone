46953af80a77e0d75827861195668fd6976bf3bd
hung-nguyen <hung-nguyen@cybozu.vn>
2024-01-22 21:29:21 +0700

    ci: categorize unit test

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -2,7 +2,12 @@
 const config = {
   roots: ["<rootDir>/src", "<rootDir>/features"],
   clearMocks: true,
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "fixtures"],
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/src/__tests__/setup.ts",
+    "fixtures"
+  ],
   testEnvironment: "allure-jest/node",
 };
 module.exports = config;
