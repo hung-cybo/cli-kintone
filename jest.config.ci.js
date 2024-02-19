9bc21a7e5cae7ee783f80aac1c13f92f7df611ce
hung-nguyen <hung-nguyen@cybozu.vn>
2024-02-19 14:12:01 +0700

    test: refactor config

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -1,13 +1,8 @@
+const { defaultConfig } = require("./jest.config");
+
 /** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  roots: ["<rootDir>/src", "<rootDir>/features"],
-  clearMocks: true,
+const config = Object.assign(defaultConfig, {
   setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/src/__tests__/setup.ts",
-    "fixtures",
-  ],
   testEnvironment: "allure-jest/node",
-};
+});
 module.exports = config;
